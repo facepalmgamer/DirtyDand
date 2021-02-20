667ee3cc2d5d0cb7b9b182db28acc7352a620f34
facepalmgamer <30478166+facepalmgamer@users.noreply.github.com>
2021-02-20 06:03:33 -0500

    added git functions to spell added ritual and source to spell finished search function

--- DirtyDand/Form1.cs
+++ DirtyDand/Form1.cs
@@ -11,6 +11,8 @@ using System.Windows.Forms;
 using static DirtyDand.Globals.GlobalVariables;
 
 using DirtyDand.Handlers;
+using DirtyDand.Properties;
+
 namespace DirtyDand
 {
     public partial class Form1 : Form
@@ -25,11 +27,91 @@ namespace DirtyDand
         private void InitializeHandlers()
         {
             SpellHandler spell = new SpellHandler();
+
         }
 
-        public async void SearchAsync(List<classes?> classes, int[] level, Components[] comps, School[] schools, Time[] time, int[] range, bool concitration, )
+        public async Task<List<Resources.Spell>> SearchAsync(/*List<classes?> classes,*/ int[] level, Components[] comps, School[] schools, Time[] time, int[] range, bool concitration, bool ritual, Source[] sources, string search)
         {
+            
+            List<Resources.Spell> temp = new List<Resources.Spell>();
+            List<Resources.Spell> temp2 = new List<Resources.Spell>();
 
+            foreach (Resources.Spell s in spellRegistry)
+                foreach (int l in level)
+                    if (l == s.GetLevel())
+                    {
+                        temp.Add(s);
+                        break;
+                    }
+            foreach (Resources.Spell s in temp)
+                if (s.GetComponents().ToArray().Equals(comps))
+                {
+                    temp2.Add(s);
+                    break;
+                }
+            temp.Clear();
+            foreach (Resources.Spell s in temp2)
+                foreach (School sc in schools)
+                    if (s.GetSchool() == sc)
+                    {
+                        temp.Add(s);
+                        break;
+                    }
+            temp2.Clear();
+            foreach (Resources.Spell s in temp)
+                foreach (Time t in time)
+                    if (s.GetTime() == t)
+                    {
+                        temp.Add(s);
+                        break;
+                    }
+            temp.Clear();
+            foreach (Resources.Spell s in temp2)
+                foreach (int r in range)
+                    if (s.GetRange() == r)
+                    {
+                        temp.Add(s);
+                        break;
+                    }
+            temp2.Clear();
+            foreach (Resources.Spell s in temp2)
+                foreach (Source b in sources)
+                    if (s.GetSource() == b)
+                    {
+                        temp2.Add(s);
+                        break;
+                    }
+            temp.Clear();
+            foreach (Resources.Spell s in temp2)
+                if (s.GetSpellName().Contains(search))
+                    temp.Add(s);
+            if (concitration)
+            {
+                temp2.Clear();
+                foreach (Resources.Spell s in temp)
+                    if (s.GetConcentration())
+                        temp2.Add(s);
+            }
+            if (ritual)
+            {
+                if (!concitration)
+                {
+                    temp2.Clear();
+                    foreach (Resources.Spell s in temp)
+                        if (s.GetRitual())
+                            temp2.Add(s);
+                    return temp2;
+                }
+                else
+                {
+                    temp.Clear();
+                    foreach (Resources.Spell s in temp2)
+                        if (s.GetRitual())
+                            temp.Add(s);
+                    return temp;
+                }
+            }
+            return temp;
 
         }
 
--- DirtyDand/Resources/Spell.cs
+++ DirtyDand/Resources/Spell.cs
@@ -1,8 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using static DirtyDand.Globals.GlobalVariables;
 
 namespace DirtyDand.Resources
@@ -10,24 +7,27 @@ namespace DirtyDand.Resources
     public class Spell
     {
 
-        private String spellName { get; }
-        private String spellDescript { get; }
-        private School school { get; }
-        private bool concentration { get; }
-        private Time time { get; }
-        private string duration { get; }
-        private int level { get; }
-        private int range { get; }
-        private List<Caster> casterList { get; }
-        private List<Components> componentsList{get;}
-        string material;
+        private String spellName;
+        private String spellDescript;
+        private School school;
+        private bool concentration;
+        private bool ritual;
+        private Time time;
+        private string duration;
+        private int level;
+        private int range;
+        private List<Caster> casterList;
+        private List<Components> componentsList;
+        private string material;
+        private Source source;
 
-        public Spell(String spellName, String spellDescript, School school, bool concentration, Time time, string duration, int level, int range, List<Caster> casterList, List<Components> componentsList, string material = "")
+        public Spell(String spellName, String spellDescript, School school, bool concentration, bool ritual, Time time, string duration, int level, int range, List<Caster> casterList, List<Components> componentsList, Source source, string material = "")
         {
             this.spellName = spellName;
             this.spellDescript = spellDescript;
             this.school = school;
             this.concentration = concentration;
+            this.ritual = ritual;
             this.time = time;
             this.duration = duration;
             this.level = level;
@@ -35,6 +35,68 @@ namespace DirtyDand.Resources
             this.casterList = casterList;
             this.componentsList = componentsList;
             this.material = material;
+            this.source = source;
+        }
+
+        public string GetSpellName()
+        {
+            return spellName;
+        }
+        public string GetSpellDescription()
+        {
+            return spellDescript;
+        }
+
+        public School GetSchool()
+        {
+            return school;
+        }
+
+        public bool GetConcentration()
+        {
+            return concentration;
+        }
+        public bool GetRitual()
+        {
+            return ritual;
+        }
+        public Time GetTime()
+        {
+            return time;
+        }
+
+        public string GetDuration()
+        {
+            return duration;
+        }
+        public int GetLevel()
+        {
+            return level;
+        }
+
+        public int GetRange()
+        {
+            return range;
+        }
+
+        public List<Caster> GetCasters()
+        {
+            return casterList;
+        }
+
+        public List<Components> GetComponents()
+        {
+            return componentsList;
+        }
+
+        public string GetMaterial()
+        {
+            return material;
+        }
+
+        public Source GetSource()
+        {
+            return source;
         }
     }
 }
