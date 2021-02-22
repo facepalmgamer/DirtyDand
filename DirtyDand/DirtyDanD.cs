6c66efa0e0c33d6da5f25c78a612fdbb89782c6b
facepalmgamer <30478166+facepalmgamer@users.noreply.github.com>
2021-02-21 19:36:12 -0500

    Removed useless code that didnt work

--- DirtyDand/DirtyDanD.cs
+++ DirtyDand/DirtyDanD.cs
@@ -13,28 +13,6 @@ namespace DirtyDand
 {
     public partial class DirtyDanD : Form
     {
-        [DllImport("User32.dll", CharSet = CharSet.Auto)]
-        public static extern int ReleaseDC(IntPtr hWnd, IntPtr hDC);
-
-        [DllImport("User32.dll")]
-        private static extern IntPtr GetWindowDC(IntPtr hWnd);
-
-        protected override void WndProc(ref Message m)
-        {
-            base.WndProc(ref m);
-            const int WM_NCPAINT = 0x85;
-            if (m.Msg == WM_NCPAINT)
-            {
-                IntPtr hdc = GetWindowDC(m.HWnd);
-                if ((int)hdc != 0)
-                {
-                    Graphics g = Graphics.FromHdc(hdc);
-                    g.FillRectangle(Brushes.Black, new Rectangle(0, 0, 4800, 23));
-                    g.Flush();
-                    ReleaseDC(m.HWnd, hdc);
-                }
-            }
-        }
         public DirtyDanD()
         {
 
